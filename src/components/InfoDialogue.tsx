f0baa47fa5a5c4bc5b5b8b064abde67e04ad3ff4
starwort <tcphone93@gmail.com>
2021-04-30 16:13:31 +0100

    Colour the git revision as I meant to the first time

--- src/components/InfoDialogue.tsx
+++ src/components/InfoDialogue.tsx
@@ -1,7 +1,7 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, useTheme } from '@material-ui/core';
 import React from 'react';
 import GitInfo from 'react-git-info/macro';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 const gitInfo = GitInfo();
 
@@ -12,12 +12,18 @@ interface InfoDialogueProps {
 
 export default function InfoDialogue(props: InfoDialogueProps) {
     const { t } = useTranslation('core');
+    const theme = useTheme();
     return <Dialog open={props.open} onClose={() => props.setOpen(false)}>
         <DialogTitle>
             {t('core:info.title')}
         </DialogTitle>
         <DialogContent>
-            {t('core:info.description', { gitRevision: gitInfo.commit.shortHash })}
+            <Trans i18nKey="core:info.description" t={t}>
+                You are currently viewing NookData revision
+                <span style={{ color: theme.palette.primary.main }}>
+                    {{ gitRevision: gitInfo.commit.shortHash }}
+                </span>
+            </Trans>
         </DialogContent>
         <DialogActions>
             <Button onClick={() => props.setOpen(false)}>
