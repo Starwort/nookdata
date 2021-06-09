42d7b7d753fd6bc6e86d2665c8098c75a235fecf
Starwort <tcphone93@gmail.com>
2021-06-10 00:33:28 +0100

    Add support link, add Japanese, refactor language dialogue and navigation drawer to make expansion easier

--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -1,11 +1,64 @@
 import { Drawer, Hidden, List, ListItem, ListItemIcon, ListItemText, Toolbar, useTheme } from '@material-ui/core';
-import { Info, Language } from '@material-ui/icons';
+import { Feedback, Info, Language } from '@material-ui/icons';
 import DarkModeIcon from '@material-ui/icons/Brightness4';
 import LightModeIcon from '@material-ui/icons/Brightness7';
 import { useTranslation } from 'react-i18next';
 import { ListItemLink } from '.';
 import { pageData } from '../pages';
 import { ThemeName } from '../themes';
+
+interface DrawerContentProps {
+    onSelect?: () => void;
+    setTheme: (theme: ThemeName) => void;
+    setInfoOpen: (value: boolean) => void;
+    setLangOpen: (value: boolean) => void;
+}
+function DrawerContent({
+    onSelect,
+    setTheme,
+    setInfoOpen,
+    setLangOpen,
+}: DrawerContentProps) {
+    const { t } = useTranslation('core');
+    const theme = useTheme();
+    const newTheme: ThemeName = theme.name === 'dark' ? 'light' : 'dark';
+    return <>
+        <List>
+            {Object.entries(pageData).map(([route, data]) => (
+                <ListItemLink key={route} to={route} icon={data.icon} primary={t(data.title)} onSelect={onSelect} />
+            ))}
+        </List>
+        <div style={{ flexGrow: 1 }} />
+        <ListItem button component="a" href="https://discord.gg/jahp5wd" target="_blank">
+            <ListItemIcon>
+                <Feedback />
+            </ListItemIcon>
+            <ListItemText primary={t('core:sidebar.support')} />
+        </ListItem>
+        <ListItem button onClick={() => { setInfoOpen(true); if (onSelect) { onSelect(); } }}>
+            <ListItemIcon>
+                <Info />
+            </ListItemIcon>
+            <ListItemText primary={t('core:sidebar.about')} />
+        </ListItem>
+        <ListItem button onClick={() => { setLangOpen(true); if (onSelect) { onSelect(); } }}>
+            <ListItemIcon>
+                <Language />
+            </ListItemIcon>
+            <ListItemText primary={t('core:sidebar.lang')} />
+        </ListItem>
+        <ListItem button onClick={() => { setTheme(newTheme); if (onSelect) { onSelect(); } }}>
+            <ListItemIcon>
+                {
+                    newTheme === 'dark'
+                        ? <DarkModeIcon />
+                        : <LightModeIcon />
+                }
+            </ListItemIcon>
+            <ListItemText primary={t('core:sidebar.theme')} />
+        </ListItem>
+    </>;
+}
 
 interface NavigationDrawerProps {
     open: boolean;
@@ -16,11 +69,10 @@ interface NavigationDrawerProps {
 }
 
 
-function NavigationDrawer(props: NavigationDrawerProps) {
+export default function NavigationDrawer(props: NavigationDrawerProps) {
     const theme = useTheme();
     const { t } = useTranslation('core');
     const container = window !== undefined ? () => window.document.body : undefined;
-    const newTheme: ThemeName = theme.name === 'dark' ? 'light' : 'dark';
     return <>
         <Hidden smUp>
             <Drawer
@@ -30,34 +82,12 @@ function NavigationDrawer(props: NavigationDrawerProps) {
                 open={props.open}
                 onClose={() => props.setOpen(false)}
             >
-                <List>
-                    {Object.entries(pageData).map(([route, data]) => (
-                        <ListItemLink key={route} to={route} icon={data.icon} primary={t(data.title)} onSelect={() => props.setOpen(false)} />
-                    ))}
-                </List>
-                <div style={{ flexGrow: 1 }} />
-                <ListItem button onClick={() => { props.setInfoOpen(true); props.setOpen(false); }}>
-                    <ListItemIcon>
-                        <Info />
-                    </ListItemIcon>
-                    <ListItemText primary={t('core:sidebar.about')} />
-                </ListItem>
-                <ListItem button onClick={() => { props.setLangOpen(true); props.setOpen(false); }}>
-                    <ListItemIcon>
-                        <Language />
-                    </ListItemIcon>
-                    <ListItemText primary={t('core:sidebar.lang')} />
-                </ListItem>
-                <ListItem button onClick={() => { props.setTheme(newTheme); props.setOpen(false); }}>
-                    <ListItemIcon>
-                        {
-                            newTheme === 'dark'
-                                ? <DarkModeIcon />
-                                : <LightModeIcon />
-                        }
-                    </ListItemIcon>
-                    <ListItemText primary={t('core:sidebar.theme')} />
-                </ListItem>
+                <DrawerContent
+                    onSelect={() => props.setOpen(false)}
+                    setTheme={props.setTheme}
+                    setInfoOpen={props.setInfoOpen}
+                    setLangOpen={props.setLangOpen}
+                />
             </Drawer>
         </Hidden>
         <Hidden xsDown>
@@ -67,36 +97,12 @@ function NavigationDrawer(props: NavigationDrawerProps) {
                 open={props.open}
             >
                 <Toolbar />
-                <List>
-                    {Object.entries(pageData).map(([route, data]) => (
-                        <ListItemLink key={route} to={route} icon={data.icon} primary={t(data.title)} />
-                    ))}
-                </List>
-                <div style={{ flexGrow: 1 }} />
-                <ListItem button onClick={() => props.setInfoOpen(true)}>
-                    <ListItemIcon>
-                        <Info />
-                    </ListItemIcon>
-                    <ListItemText primary={t('core:sidebar.about')} />
-                </ListItem>
-                <ListItem button onClick={() => props.setLangOpen(true)}>
-                    <ListItemIcon>
-                        <Language />
-                    </ListItemIcon>
-                    <ListItemText primary={t('core:sidebar.lang')} />
-                </ListItem>
-                <ListItem button onClick={() => props.setTheme(newTheme)}>
-                    <ListItemIcon>
-                        {
-                            newTheme === 'dark'
-                                ? <DarkModeIcon />
-                                : <LightModeIcon />
-                        }
-                    </ListItemIcon>
-                    <ListItemText primary={t('core:sidebar.theme')} />
-                </ListItem>
+                <DrawerContent
+                    setTheme={props.setTheme}
+                    setInfoOpen={props.setInfoOpen}
+                    setLangOpen={props.setLangOpen}
+                />
             </Drawer>
         </Hidden>
     </>;
 }
-export default NavigationDrawer;
