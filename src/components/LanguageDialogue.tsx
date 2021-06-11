2de3275c2cd071d873ac67f313e99ec2583ec33a
Starwort <starwort.computing@gmail.com>
2021-06-11 03:17:09 +0100

    This took far too long and I've run out of energy - Add Japanese localisations for every bug and a large portion of Critterpedia's interface - Move supportedLngs and the dialogue to have a single source of truth - Remove inconsistent <br> tags

--- src/components/LanguageDialogue.tsx
+++ src/components/LanguageDialogue.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogTitle, List, ListItem, ListItemText } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
+import { availableLocalisations } from "../i18n";
 
 interface LanguageDialogueProps {
     open: boolean;
@@ -11,7 +12,7 @@ export default function LanguageDialogue(props: LanguageDialogueProps) {
     return <Dialog aria-labelledby="lang-dialogue-title" open={props.open} PaperProps={{ style: { width: 250 } }}>
         <DialogTitle style={{ textAlign: 'center' }} id="lang-dialogue-title">{t('core:lang.choose')}</DialogTitle>
         <List>
-            {['en', 'fr', 'ja'].map(lang =>
+            {availableLocalisations.map(lang =>
                 <ListItem key={lang} style={{ textAlign: 'center' }} button onClick={() => props.setLang(lang)}>
                     <ListItemText primary={t(`core:lang.${lang}`)} />
                 </ListItem>
